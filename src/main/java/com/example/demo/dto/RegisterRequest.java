0e2a3fa44e05e5762ca1fc5e39e7ada69441461d
urido <doreoriol@gmail.com>
2026-03-17 14:10:37 +0100

    feat: add validation pattern to password creation

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -2,6 +2,7 @@ package com.example.demo.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,5 +22,9 @@ public class RegisterRequest {
 
     @NotBlank
     @Size(min = 8, max = 72)
+    /* @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,72}$",
+            message = "Password must contain at least: 1 uppercase, 1 lowercase, 1 number, 1 special character"
+    ) */
     private String password;
 }
